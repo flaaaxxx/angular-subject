89601aac77a210a86745a37e063f355ae7f62247
flaaaxxx <flaaaxxx@gmail.com>
2021-09-26 19:46:43 +0200

    Example of using observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-subject';
+
+  ngOnInit(): void {
+
+    ////////////////
+    // create stream
+    const observable = interval(1000);
+
+    observable.subscribe(x => console.log('Subscriber 1: ' + x));
+
+    setTimeout(() => observable.subscribe(x => console.log('Subscriber 2: ' + x)), 4000);
+
+  }
+
 }
